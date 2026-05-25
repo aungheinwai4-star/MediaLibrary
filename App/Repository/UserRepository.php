9f1abccf2719959ca0e7ad29e864f3e7e85a7593
Kaung Pyae Thein Tun <kaungpyaetheintun@Kaungs-MacBook-Air.local>
2026-05-25 14:24:07 +0630

    Fix user repo.

--- App/Repository/UserRepository.php
+++ App/Repository/UserRepository.php
@@ -6,12 +6,6 @@ use App\Contract\UserRepositoryInterface;
 
 class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
-    protected string $table = 'users';
-
-    protected array $searchColumns = ['name', 'email'];
-
-    protected string $primaryKey = 'id';
-
     public function findByEmail(string $email): ?array
     {
         return $this->fetchOne('SELECT * FROM users WHERE email = ?',[$email]);
